35f90dc391add2c3071c7ac37e707320af2075a9
Dina <Dina@LAPTOP-H7SGRNL2>
2024-07-09 09:44:13 +0300

    Add project files.

---
+++ WebApplication3/Models/RegisteredCourse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class RegisteredCourse
+    {
+        public int User_ID { get; set; }
+        public int Course_ID { get; set; }
+        public string Attendance_Status { get; set; }
+        public string Evaluation_Status { get; set; }
+        public string Certificate_Status { get; set; }
+        public virtual User User { get; set; }
+        public virtual CourseSchedule CourseSchedule { get; set; }
+    }
+}
---
+++ WebApplication3/Models/TrainingSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class TrainingSystemContext : DbContext
+    {
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<SupportTicket> SupportTickets { get; set; }
+        public DbSet<CourseClassification> CourseClassifications { get; set; }
+        public DbSet<CertificateTemplate> CertificateTemplates { get; set; }
+        public DbSet<CourseSchedule> CourseSchedules { get; set; }
+        public DbSet<RegisteredCourse> RegisteredCourses { get; set; }
+    }
+}
